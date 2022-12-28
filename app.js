261d4854ca92b35dc19493153873fb3c5127aa3b
Shadowa <85016881+Shadowa123Baran@users.noreply.github.com>
2022-12-29 00:17:30 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -18,3 +18,54 @@ const utkaSecretElem = document.querySelector('#utka')
 utkaBtnElem.addEventListener('click', () => {
     utkaSecretElem.classList.toggle('infoAbout__about-utka-text')
 })
+
+const erubyBtnElem = document.querySelector('.infoAbout__blank-eruby')
+const erubySecretElem = document.querySelector('#eruby')
+const erubyBorderElem = document.querySelector('.eruby__border')
+
+erubyBtnElem.addEventListener('click', () => {
+    erubySecretElem.classList.toggle('infoAbout__about-eruby-text')
+    erubyBtnElem.classList.toggle('eruby__border')
+})
+
+const zulusBtnElem = document.querySelector('.infoAbout__blank-zulus')
+const zulusSecretElem = document.querySelector('#zulus')
+
+zulusBtnElem.addEventListener('click', () => {
+    zulusSecretElem.classList.toggle('infoAbout__about-zulus-text')
+})
+
+const hidaBtnElem = document.querySelector('.infoAbout__blank-hida')
+const hidaSecretElem = document.querySelector('#hida')
+
+hidaBtnElem.addEventListener('click', () => {
+    hidaSecretElem.classList.toggle('infoAbout__about-hida-text')
+})
+
+const stasBtnElem = document.querySelector('.infoAbout__blank-stas')
+const stasSecretElem = document.querySelector('#stas')
+
+stasBtnElem.addEventListener('click', () => {
+    stasSecretElem.classList.toggle('infoAbout__about-stas-text')
+})
+
+const shadBtnElem = document.querySelector('.infoAbout__blank-shad')
+const shadSecretElem = document.querySelector('#shad')
+
+shadBtnElem.addEventListener('click', () => {
+    shadSecretElem.classList.toggle('infoAbout__about-shad-text')
+})
+
+const nighBtnElem = document.querySelector('.infoAbout__blank-nigh')
+const nighSecretElem = document.querySelector('#nigh')
+
+nighBtnElem.addEventListener('click', () => {
+    nighSecretElem.classList.toggle('infoAbout__about-nigh-text')
+})
+
+const bisBtnElem = document.querySelector('.infoAbout__blank-bis')
+const bisSecretElem = document.querySelector('#bis')
+
+bisBtnElem.addEventListener('click', () => {
+    bisSecretElem.classList.toggle('infoAbout__about-bis-text')
+})
